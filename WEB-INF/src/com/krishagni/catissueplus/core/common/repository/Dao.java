181030dc295005deb4e3445f6dd79bcf0f6da3b7
ajaysamgir <ajaysamgir@bitbucket.org>
2014-10-17 18:52:55 +0530

    remove src directory

--- WEB-INF/src/com/krishagni/catissueplus/core/common/repository/Dao.java
+++
@@ -1,9 +0,0 @@
-
-package com.krishagni.catissueplus.core.common.repository;
-
-public interface Dao<T> {
-
-	public void saveOrUpdate(T t);
-	
-	public void delete(T t);
-}
--- WEB-INF/src/com/krishagni/catissueplus/core/common/util/ObjectType.java
+++
@@ -1,6 +0,0 @@
-
-package com.krishagni.catissueplus.core.common.util;
-
-public enum ObjectType {
-	PARTICIPANT;
-}
