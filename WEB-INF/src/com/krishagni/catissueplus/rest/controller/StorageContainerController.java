625febd5066b95df5d435a5626005865e4afe046
Mosin <mosin.kazi@krishagni.com>
2014-11-14 19:33:23 +0530

    added missing checkins

--- WEB-INF/src/com/krishagni/catissueplus/rest/controller/StorageContainerController.java
+++ WEB-INF/src/com/krishagni/catissueplus/rest/controller/StorageContainerController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,10 +16,15 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.krishagni.catissueplus.core.administrative.events.ReqStorageContainerEvent;
 import com.krishagni.catissueplus.core.administrative.events.ReqStorageContainersEvent;
+import com.krishagni.catissueplus.core.administrative.events.ScanStorageContainerDetails;
+import com.krishagni.catissueplus.core.administrative.events.ScanStorageContainerDetailsEvents;
 import com.krishagni.catissueplus.core.administrative.events.StorageContainerEvent;
 import com.krishagni.catissueplus.core.administrative.events.StorageContainerSummary;
 import com.krishagni.catissueplus.core.administrative.events.StorageContainersEvent;
 import com.krishagni.catissueplus.core.administrative.services.StorageContainerService;
+import com.krishagni.catissueplus.core.biospecimen.events.SpecimenDetail;
+import com.krishagni.catissueplus.core.biospecimen.events.SpecimenUpdatedEvent;
+import com.krishagni.catissueplus.core.biospecimen.events.UpdateSpecimenEvent;
 
 @Controller
 @RequestMapping("/storage-containers")
@@ -57,4 +63,26 @@ public class StorageContainerController {
 		
 		return resp.getContainer();
 	}
+	
+	@RequestMapping(method = RequestMethod.POST, value = "/validateScanData")
+ @ResponseStatus(HttpStatus.OK)
+ @ResponseBody
+ public ScanStorageContainerDetails validateScan(@RequestBody ScanStorageContainerDetails ScanStorageContainerDetails) {
+	  ScanStorageContainerDetailsEvents resp = storageContainerSvc.validateAndPopulateScanContainerData(ScanStorageContainerDetails);
+	  if (!resp.isSuccess()) {
+	    resp.raiseException();
+	   }	   
+	   return resp.getContainerDetails();
+ }
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/scanContainer")
+ @ResponseStatus(HttpStatus.OK)
+ @ResponseBody
+ public ScanStorageContainerDetails scanContainerData() {
+   ScanStorageContainerDetailsEvents resp = storageContainerSvc.getScanContainerData();
+   if (!resp.isSuccess()) {
+     resp.raiseException();
+    }    
+    return resp.getContainerDetails();
+ }
 }
